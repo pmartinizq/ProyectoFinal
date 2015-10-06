e0105ff2f3a96e796ecf5bd612af30e6d7d5db3f
Gabriel <pmartin.izq@gmail.com>
2015-10-06 19:30:31 -0300

    ultimos cambios en el RTCmodule.c

--- Soft V1.0/Firmware 1.0/Sources/RTCmodule.c
+++ Soft V1.0/Firmware 1.0/Sources/RTCmodule.c
@@ -1,8 +1,11 @@
 #include "RTCmodule.h"
 
+
 interrupt VectorNumber_Vrtc   void RTCInterrupt (void){
 uint8_t i=0;
+uint8_t lastStatus;
 RTCSC=RTCSC|0x80;
+tickTime++;
 kbiTime++;
 adcTime++; /*
 if(kbiTime==kbiSampleFreq){      //5000 para bajas velocidades
@@ -14,6 +17,14 @@ if(adcTime==adcSampleFreq){      //5000 para bajas velocidades
   ADCSC1=11100000;
   adcTime=0;
 }  */
+
+if(STATUS_MEF==lastStatus){
+   STATUS_MEF=MEF_WAIT; 
+  
+}
+lastStatus=STATUS_MEF;
+
+
 for(i=0;i<=getExecutingVectorPointer();i++){
   decreaseTimer(getFromExecutingVectorOnIndex(i));
      
@@ -23,15 +34,19 @@ for(i=0;i<=getExecutingVectorPointer();i++){
 }
 
 void decreaseTimer(FunctionStruct* currentFunction){
-  if((currentFunction->status==RUNNING)&&(currentFunction->timerCount==0)){
-        currentFunction->status=TIMEOUT;
-        
-  } 
- if((currentFunction->status==READY)&&(currentFunction->timerCount==0)){
-       currentFunction->status=INACCESSIBLE_DEVICE;
+  
+  if(currentFunction->timerCount!=NO_TIMER){
+    
+    if((currentFunction->status==RUNNING)&&(currentFunction->timerCount==0)){
+          currentFunction->status=TIMEOUT;
+          
+    } 
+   if((currentFunction->status==READY)&&(currentFunction->timerCount==0)){
+         currentFunction->status=INACCESSIBLE_DEVICE;
+    }
+    if((currentFunction->status==RUNNING)||(currentFunction->status==READY&&currentFunction->timerCount>0)){
+          currentFunction->timerCount=currentFunction->timerCount-1;
+     } 
   }
-  if((currentFunction->status==RUNNING&&currentFunction->timerCount>5)||(currentFunction->status==READY&&currentFunction->timerCount>0)){
-        currentFunction->timerCount=currentFunction->timerCount-1;
-   }  
    
 }
